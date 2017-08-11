d292fdd7bdb42b95c73d19dc1b762a7dc0522b00
Paolo T. Inocencion <paoloinocencion@gmail.com>
2017-08-10 21:16:54 -0400

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,67 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class Solution {
+    //
+    static void levelOrder(Node root) {
+        //Write your code here
+        Queue<Node> queue = new LinkedList();
+        // enqueue current root
+        queue.add(root);
+
+        // while there are nodes to process
+        while (!queue.isEmpty()) {
+            // dequeue next node
+            Node current = queue.remove();
+            System.out.print(current.data + " ");
+
+            // enqueue child elements from next level in order
+            if (current.left != null) {
+                queue.add(current.left);
+            }
+            if (current.right != null) {
+                queue.add(current.right);
+            }
+        }
+    }
+
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        levelOrder(root);
+    }
+}
